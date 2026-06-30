87ac34d1f2eff89ffeb882968a603accbc25395b
quangtynu <quangbq@gmail.com>
2026-06-30 11:02:21 +0700

    fix

--- src/plugin/extensions/providers/models/custom-models.generated.ts
+++ src/plugin/extensions/providers/models/custom-models.generated.ts
@@ -1,6 +1,6 @@
 // THIS FILE IS AUTO-GENERATED
 // Source: https://models.dev/api.json
-// Generated: 2026-06-29T15:43:47.532Z
+// Generated: 2026-06-30T01:48:35.996Z
 // DO NOT EDIT MANUALLY - Run 'npm run generate-custom-models' to update
 
 import type { Model } from "@earendil-works/pi-ai";
